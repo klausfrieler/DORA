c6ee88230d0189199bd9e9d79441996d44732914
klaus <kgf@omniversum.de>
2022-04-04 19:45:30 +0200

    Some refactoring, finishing touches

--- analysis.R
+++ analysis.R
@@ -1,57 +1,91 @@
 library(tidyverse)
 library(dtw)
 
+tempos <- c("fa" = .4, "sl" = .6)
+max_times <- tempos*c(50, 33)
+
 get_timeline_parameters <- function(timeline, time_base){
   t0 <- timeline[1]
   t_max <- ceiling(diff(range(timeline))/time_base) * time_base
   c("t0" = t0, "t_max" = t_max, "time_base" = time_base, n = length(timeline))
 }
 
-get_isochronous_envelope <- function(query, time_base, remove_offset = F){
+get_isochronous_envelope <- function(query, time_base){
   seq(floor(min(query)), ceiling(max(query)), time_base)
 }
 
-get_best_alignment <- function(query, target, summary = F){
+#' get_best_alignment: Calc best DTW alignment between to timelines and optional features derived from this 
+#'
+#' @param query <dbl> List of onsets
+#' @param target <dbl> List of onsets 
+#' @param summary <lgl> Returned summary?
+#'
+#' @return Either a tibble object with a query -> target mapping or summary features
+#' @export
+#'
+#' @examples
+get_best_alignment <- function(query, target){
+  #get the DTW alignment
   align <- dtw(query, target) 
   ret <- c()
+  
+  #find the best alignment by selecting only the closest time points in case of multiple mappings
   ba <- 
     map_dfr(1:length(query), function(i){
-    target_idz <- align$index2[which(align$index1 == i)]
-    candidates <- target[target_idz]
-    candidate_pos <- paste(which(align$index1 == i), collapse = ";")
-    
-    abs_diff <- abs(candidates - query[i])
-    best_idx <- which.min(abs_diff)
-    best <- min(abs_diff)
-    target_best <- target_idz[best_idx ]
-    
-    tibble(query_pos = i, target_pos = target_best, 
-           candidate_pos = candidate_pos, 
-           query = query[i], target = target[target_best], d = best[1])
+      target_idz <- align$index2[which(align$index1 == i)]
+      candidates <- target[target_idz]
+      candidate_pos <- paste(which(align$index1 == i), collapse = ";")
+      
+      abs_diff <- abs(candidates - query[i])
+      best_idx <- which.min(abs_diff)
+      best <- min(abs_diff)
+      target_best <- target_idz[best_idx ]
+      
+      tibble(query_pos = i, 
+             target_pos = target_best, 
+             candidate_pos = candidate_pos, 
+             query = query[i], 
+             target = target[target_best], 
+             d = best[1])
   })
-  #browser()
-  if(summary){
-    ba <- tibble(MAE = mean(ba$d), MAS = sd(ba$d), 
-                 norm_dist = align$normalizedDistance, 
-                 d_n = length(target) - length(query))
-  }
-  ba  
+  # MAE mean absolute error; mean of absolute differences between query time points and best target timepoints
+  # MAS mean absolute error stddev; Sd of absolute differences between query time points and best target timepoints
+  # norm_dist: normaliized DTW distance
+  # d_n: difference in length between target and query, if positive, query contains *more* events than target
+  summary <- tibble(MAE = mean(ba$d),
+               MAS = sd(ba$d), 
+               MAX = max(ba$d),
+               norm_dist = align$normalizedDistance, 
+               d_n = length(query) - length(target))
+  list(raw = ba, summary = summary)  
 }
 
 get_circular_features <- function(onsets, time_base, remove_offset = F){
   require(circular)
-  #browser()
+  #remove offset, i.e. moving first onset to zero  
   if(remove_offset){
     onsets <- onsets - onsets[1]
   }
+  #transform onsets to cicrular variables, based on time_base as periodictiy.
+  # onsets which are separated by multiples of the time_base ar mapped to the same point on
+  # the 2d circle
   circ_onsets <- circular::circular(2 * pi * onsets / time_base)
   
+  #circular mean is the angle of the resultant, which is a complex number (2d vector)
+  #measure for mean phase of onsets in regard to a oscillation of
+  #circular standard devation is square root of the logarithm of the inverse square length of the resultant vector, 
+  #values between 0 and Inf. Uniform distributions on the circle whill have the highest Circular Sd, as the resultant
+  # is close to the center of the circular, so it has a very short length, i.e. the inverse square length is large
+  #asynchrony is simple the sign of the circular mean
   tibble(circ_mean = as.numeric(circular::mean.circular(circ_onsets)), 
          circ_sd = as.numeric(circular::sd.circular(circ_onsets)),
-         neg_async = sign(circ_mean))
+         #the other two options correlate highly circular sd
+         #circ_var = as.numeric(circular::var.circular(circ_onsets)),
+        #circ_disp = circ_var/(2* (1 - circ_var))^2,
+         asynchrony = sign(circ_mean))
 }
 
-get_iso_features <- function(onset_data){
+get_iso_features <- function(onset_data, remove_offset = F, cut_extra_beats = T){
   base_data <- onset_data %>% 
     distinct(experimenter, 
              age_group, condition, 
@@ -65,40 +99,217 @@ get_iso_features <- function(onset_data){
              sd_ioi,
              cv_ioi) 
   tids <- unique(onset_data$trial_id)
-  tempos <- c("fa" = .4, "sl" = .6)
+  #iterate over all trials
   map_dfr(tids, function(tid){
     tmp <- onset_data %>% filter(trial_id == tid) 
     query <- tmp %>% pull(onset)
     offset <- query[1]
-    query <-  query - offset
-    time_base <- tempos[tmp$tempo[1]] 
-    iso_env <- get_isochronous_envelope(query, time_base)
-    circ_features <- get_circular_features(query, time_base, remove_offset = T)
     
-    alignment_features <- get_best_alignment(query, iso_env, summary = T) %>% 
+    #remove offset if requested
+    if(remove_offset) query <-  query - offset
+    
+    #time_base is just the (inverse) tempo
+    time_base <- tempos[tmp$tempo[1]]
+    
+    #if requested, disregard beatsbeyond the maximum time of the trials, as participant sometime simple played on
+    if(cut_extra_beats){
+      #browser()
+      n_before <- length(query)
+      mt_before <- max(query)
+      query <- query[query <= max_times[tmp$tempo[1]]]
+      messagef("[%s] Cut query from %d to %d (d = %d, t_before = %.3f, tempo = %s)", 
+               tid,
+               n_before, length(query), 
+               n_before - length(query),
+               mt_before, tmp$tempo[1])
+    }
+    #we generate a isochronous timeline for comparison (isochronous envelope)
+    iso_env <- get_isochronous_envelope(query, time_base)
+    
+    #first circular features
+    circ_features <- get_circular_features(query, time_base, remove_offset = remove_offset)
+    
+    #then alignment base features
+    best_aligment <- get_best_alignment(query, iso_env)
+    alignment_features <-  best_aligment$summary %>% 
       mutate(trial_id = tid, 
              offset = offset,
-             mod_offset = offset %% time_base,
              time_base = time_base)
-    bind_cols(alignment_features, circ_features)
+    
+    #combined the features
+    bind_cols(alignment_features, circ_features) %>% 
+      mutate(removed_offset = remove_offset)
   }) %>% 
-    select(trial_id, time_base, offset, mod_offset, MAE, MAS, d_n, norm_dist, everything()) %>% 
+    select(trial_id, time_base, offset, MAE, MAS, d_n, norm_dist, everything()) %>% 
     left_join(base_data, by = "trial_id")
   
 }
 
-get_production_features <- function(onsets, target_rhythm, isochronous = F){
-  ret <- NULL
-  if(isochronous){
-    #ret <- make cool circular stuff
-  }
-  ret
-}
-get_all_production_features <- function(data_dir, target_data){
-  #read all files
-  #read_all_rhythm_data(data_dir)
-  #for all rhythm get features
-  #put it all togeher
+get_ref_rhythm_for_trial <- function(trial_id, stimulus_data){
+  tid <- parse_filename(trial_id, type = "rhythm_prod")
+  code <- stimulus_data$design %>% 
+    filter(p_id == tid$condition, rhythm == tid$rhythm, setting == tid$setting) %>% 
+    pull(code)
+  ref_rhythm <- stimulus_data$rhythms %>% 
+    filter(code == !!code, rhythm_id == tid$rhythm)
+  ref_rhythm    
   
-  #ertun data
+}
+
+check_rhythm <- function(trial_id, onset_data, stimulus_data, remove_offset = T, plot = F){
+  browser()
+  query <- onset_data %>% filter(trial_id == !!trial_id) %>% pull(onset)
+  if(remove_offset){
+    query <- query - query[1]  
+  }
+  ref_rhythm <- get_ref_rhythm_for_trial(trial_id, stimulus_data) %>% 
+    mutate(running_beat = (bar -1) * period + (beat -1)  )
+  ref_ibi <- 60/69
+  #print(ref_rhythm)
+  target <- ref_rhythm %>% pull(onset)
+  target <- target/.5 * ref_ibi 
+  query_norm <- query/ref_ibi * .5
+  #print(query_norm)
+  ba <- get_best_alignment(query, target)
+  #print(ba)
+  tempo_est <- tibble(trial_id = trial_id, mean_ibi = NA, sd_ibi = NA, ibi_diff = NA, ibi_diff_rel = NA)
+  #browser()
+  if(nrow(ba$raw) == length(query)){
+    on_beats <- which(ref_rhythm$tatum == 1)
+    if(length(on_beats) != 0){
+      #browser()
+      beat_dur <- diff(ref_rhythm[on_beats,]$running_beat)
+      ibis <- diff(query[on_beats])/beat_dur 
+      tempo_est <- tibble(trial_id  = trial_id, 
+                          mean_ibi = mean(ibis), 
+                          sd_ibi = sd(ibis),
+                          ibi_diff = mean_ibi - ref_ibi,
+                          ibi_diff_rel = round(100*(mean_ibi - ref_ibi)/ref_ibi, 1))
+      #print(tempo_est)
+    }
+  }
+  if(plot){
+    q <- plot_dtw_alignment(query, target) + scale_x_continuous(breaks = round(seq(1:3) * ref_ibi, 3), labels = 1:3)
+    print(q)
+  }
+  browser()
+  tempo_est 
+}
+
+# get_social_features <- function(onset_data, stimulus_data, type = "rhythm_prod"){
+#   base_data <- onset_data %>%
+#     distinct(experimenter,
+#              age_group,
+#              condition,
+#              source,
+#              rhythm,
+#              setting,
+#              p_id,
+#              trial_id,
+#              n_onsets) %>% 
+#     left_join(stimulus_data$design %>% select(condition = p_id, setting,  rhythm, code),
+#               by = c("condition", "setting", "rhythm"))
+#   
+#   type <- match.arg(type)
+#   data <- onset_data %>% filter(setting == "so")
+#   p_trials <- onset_data %>% filter(source == "pa") %>% pull(trial_id) %>% unique()
+#   map_dfr(p_trials, function(ptid){
+#     x_trial <- onset_data %>% filter(trial_id == str_replace(ptid, "pa", "ex"))
+#     if(nrow(x_trial) == 0){
+#       messagef("No match found for %s", ptid)
+#       return(NULL)
+#     }
+#     ref_rhythm <- stimulus_data$rhythms %>% filter(code == 
+#             base_data %>% filter(trial_id == ptid) %>% pull(code))
+#     query <- onset_data %>% filter(trial_id == ptid)%>% pull(onset)
+#     target <-  x_trial %>% pull(onset)
+#     q <- plot_dtw_alignment(query - query[1], target - target[1])
+#     print(q)
+#     browser()
+#     alignment_features <- get_best_alignment(query - query[1], target - target[1], summary = T) %>%
+#       mutate(trial_id = ptid)
+#     print(alignment_features)
+#     alignment_features
+#   })
+# }
+  
+
+get_rhythm_features <- function(onset_data, stimulus_data){
+  base_data <- onset_data %>%
+    distinct(experimenter,
+             age_group,
+             condition,
+             source,
+             rhythm,
+             setting,
+             p_id,
+             trial_id,
+             n_onsets) %>% 
+    left_join(stimulus_data$design %>% select(condition = p_id, setting,  rhythm, code),
+              by = c("condition", "setting", "rhythm"))
+  
+  tids <- unique(base_data$trial_id)
+  
+  #tempo was the same in all trials
+  tempo <- 60/69
+  
+  map_dfr(tids, function(tid){
+    #get query data
+    tmp <- onset_data %>% filter(trial_id == tid) 
+    query <- tmp %>% pull(onset)
+    offset <- query[1]
+    
+    #as production task was serial, absolute phase is of no interest, so remove offset 
+    query <- query - offset
+    
+    #find rhythm (class of variants) and code of target rhythm (actual variant of rhythm)
+    rhythm <- base_data %>% filter(trial_id == tid) %>% pull(rhythm)
+    code <- base_data %>% filter(trial_id == tid) %>%  pull(code)
+    
+    #find target rhythm and extract max subdivision for 
+    #calculating time_base = tatum duration 
+    ref_rhythm <- stimulus_data$rhythms %>% filter(code == !!code)
+    time_base <- tempo/max(ref_rhythm$division) 
+    
+    #rescale  tempo of reference rhythm (which has  120 bpm)
+    target <- ref_rhythm$onset / .5 * tempo
+    
+    messagef("Calculating features for trial %s, time base (raw) = %.1f, time base = %.3f, divison = %d, code = %s", tid,  time_base/max(ref_rhythm$division), time_base, max(ref_rhythm$division), code)
+    
+    #Calc cicrcular features beased on tatums, not bsaed on beat an in the isochronous case! 
+    #Attention! Might have non-linear numerical ramifications for different tatums, save tatum and time_base 
+    
+    circ_features <- get_circular_features(query, time_base) %>% 
+      mutate(division = max(ref_rhythm$division), time_base = time_base)
+    
+    #get alignmenat features as in the isochronous case.
+    best_alignment <-  get_best_alignment(query, target) 
+                                              
+    alignment_features <- best_alignment$summary %>%
+      mutate(trial_id = tid,
+             offset = offset,
+             code = code,
+             rhythm = rhythm)
+    #get tempo estimation
+    #browser()
+    tempo_est <- tibble(mean_ibi = NA, sd_ibi = NA, ibi_diff = NA, ibi_diff_perc = NA)
+    if(nrow(best_alignment$raw) == length(query)){
+      on_beats <- which(ref_rhythm$tatum == 1)
+      if(length(on_beats) != 0){
+        #browser()
+        beat_dur <- diff(ref_rhythm[on_beats,]$running_beat)
+        ibis <- diff(query[on_beats])/beat_dur 
+        tempo_est <- tibble(mean_ibi = mean(ibis), 
+                            sd_ibi = sd(ibis),
+                            ibi_diff = mean_ibi - tempo,
+                            ibi_diff_perc = round(100*(mean_ibi - tempo)/tempo, 1))
+      }
+    }
+    
+    #binding features together
+    bind_cols(alignment_features, circ_features, tempo_est)
+  }) %>% 
+    select(trial_id, offset, MAE, MAS, d_n, norm_dist, everything()) %>% 
+    left_join(base_data, by = c("trial_id", "rhythm", "code"))
+  
 }
--- read_data.R
+++ read_data.R
@@ -1,66 +1,174 @@
 library(tidyverse)
 source("analysis.R")
+source("dtw_util.R")
 
-parse_filename <- function(fname){
+messagef <- function(...) message(sprintf(...))
+
+parse_filename <- function(fname, type = c("iso", "rhythm_prod", "rhythm_def")){
+  type <- match.arg(type)
+  
   fname <- fname %>% basename() %>% str_replace(".csv", "") 
   elements <- str_split(fname, "_")
-  map_dfr(elements, function(x){
-    tibble(experimenter = x[1], 
-           age_group = x[2],
-           condition = x[3],
-           serial = x[4],
-           source = x[5],
-           tempo = x[6],
-           setting = x[7],
-           p_id = paste(x[1:4], collapse = "_"),
-           trial_id = paste(x, collapse = "_")
-           )
-  })
+  if(type == "iso"){
+    map_dfr(elements, function(x){
+      tibble(experimenter = x[1], 
+             age_group = x[2],
+             condition = x[3],
+             serial = x[4],
+             source = x[5],
+             tempo = x[6],
+             setting = x[7],
+             p_id = paste(x[1:3], collapse = "_"),
+             trial_id = paste(x, collapse = "_")
+             )
+    })
+  } 
+  else if(type == "rhythm_prod"){
+    map_dfr(elements, function(x){
+      tibble(experimenter = x[1], 
+             age_group = x[2],
+             condition = x[3],
+             serial = x[4],
+             source = x[5],
+             rhythm = x[6],
+             setting = x[7],
+             p_id = paste(x[1:3], collapse = "_"),
+             trial_id = paste(x, collapse = "_"))
+      })
+  }
+  else if(type == "rhythm_def"){
+    map_dfr(elements, function(x){
+      tibble(rhythm_id = x[1], 
+             code = x[2])
+    })
+  }
 }
 
-get_basic_stats <- function(onset){
+get_basic_stats <- function(onset, type = c("iso", "rhythm_prod")){
   iois <- diff(onset)
-  tibble(mean_ioi = mean(iois, na.rm = T),
-         med_ioi = median(iois, na.rm = T),
-         sd_ioi = sd(iois, na.rm = T),
-         cv_ioi = sd_ioi/mean_ioi, 
-         t0 = onset[1]) 
+  type <- match.arg(type)
+  if(type == "iso"){
+    tibble(mean_ioi = mean(iois, na.rm = T),
+           med_ioi = median(iois, na.rm = T),
+           sd_ioi = sd(iois, na.rm = T),
+           log_sd_ioi = log(sd(iois, na.rm = T)),
+           cv_ioi = sd_ioi/mean_ioi, 
+           t0 = onset[1]) 
+    
+  }
+  else{
+    tibble(t0 = onset[1]) 
+  }
 }
 
-read_all_files <- function(data_dir){
+read_all_files <- function(data_dir, type = c("iso", "rhythm_prod")){
+  type <- match.arg(type)
   files <- list.files(data_dir, pattern = "*.csv", full.names = T)
   map_dfr(files, 
           function(fn){
-            messagef("Reading %s", fn)
-            browser()
-            tmp <- read.csv(fn, header = F, stringsAsFactors = F) %>% 
-              as_tibble() %>% 
-              bind_cols(parse_filename(fn))
-            }) %>% 
-    select(-V2) %>% 
+          #messagef("Reading %s", fn)
+          #browser()
+          tmp <- data.table::fread(fn) %>% 
+            as_tibble() %>% 
+            bind_cols(parse_filename(fn, type))
+          tmp$V2 <- NULL
+          tmp
+          }) %>% 
     rename(onset =  V1) %>% 
-    group_by(trial_id) %>% 
+    group_by(trial_id) %>%
     mutate(n_onsets = n()) %>% 
-    mutate(get_basic_stats(onset)) %>% 
+    mutate(get_basic_stats(onset, type = type)) %>% 
     ungroup()
 }
 
-setup_workspace <- function(iso_data_dir, rhythm_data_dir = NULL, reread_data = FALSE){
+setup_rhythm_data <- function(rhythm_stim_dir = "rhythms_simple2", 
+                              fname_design = "rhythms_simple2/design/experimental_design_rhythm_simple.csv"){
+  rhythm_design = read.csv(fname_design, sep = ";", stringsAsFactors = F) %>% 
+    as_tibble() 
+  
+  patch_rhythms <- rhythm_design %>% 
+    group_by(rhythm) %>% 
+    mutate(has_na = any(is.na(code))) %>% 
+    ungroup() %>% 
+    filter(has_na,!is.na(code)) %>% 
+    distinct(rhythm, patch_code = code) 
+
+  rhythm_design <- rhythm_design %>% left_join(patch_rhythms, by = "rhythm")
+  rhythm_design[is.na(rhythm_design$code),]$code <- rhythm_design[is.na(rhythm_design$code),]$patch_code
+  rhythm_design <- rhythm_design %>% 
+    filter(modality %in% c(2,5)) %>% 
+    mutate(p_id = sprintf("%02d", p_id),
+           setting = factor(modality, levels = c(2, 5), labels = c("ac", "so")),
+           rhythm = factor(rhythm,
+                           levels = 1:10,
+                           labels = c('i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix', 'x'))) %>%
+  select(-modality, -midi_file, -variant, -patch_code)
+    
+  rf <- list.files(rhythm_stim_dir, pattern = "*.csv", full.names = T)
+  rhythm_data <- 
+    map_dfr(rf, function(fn){
+      data.table::fread(fn) %>% 
+        as_tibble() %>% 
+        select(onset, bar, beat, tatum, division, period) %>% 
+        mutate(running_beat = (bar - 1) * period + (beat - 1)) %>% 
+        bind_cols(parse_filename(fn, type = "rhythm_def"))
+    }) %>% 
+    mutate(rhythm_id = factor(rhythm_id, 
+                              labels = c('i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix', 'x')))
+  list(design = rhythm_design, rhythms = rhythm_data)
+}
+
+post_process <- function(data, data_type = c("onsets", "features"), type = c("iso", "rhythm_prod")){
+  data_type <- match.arg(data_type)
+  type <- match.arg(type)
+  if(type == "iso"){
+    exclude_trials <- data %>% filter(source == "ex", sd_ioi > .05) %>% distinct(trial_id)   
+  }
+  data <- data %>% filter(source != "ex")
+}
+
+setup_workspace <- function(iso_data_dir = "data/drumking", rhythm_data_dir = "data/rhythm_prod", reread_data = FALSE){
+  messagef("Reading stimulus and design data")
+  stimulus_data <- setup_rhythm_data()
+  assign("stimulus_data", stimulus_data, globalenv())
+  
   if(reread_data){
-    messagef("Importing all data fro %s", iso_dat_dir)
-    all_data_iso <- read_all_files(iso_data_dir)
-    #all_data_rhythm <- read_all_files("rhythm")
-    saveRDS(all_data_iso, file.path(iso_data_dir, "all_data_ioi.rds"))
+    messagef("Importing all iso data from %s", iso_data_dir)
+    iso_data <- read_all_files(iso_data_dir, type = "iso") %>% filter(source != "ro")
+    saveRDS(iso_data, file.path(iso_data_dir, "iso_data.rds"))
+    
+    messagef("Calculating iso features...")
+    iso_features <-  get_iso_features(iso_data) 
+    assign("iso_features", iso_features, globalenv())
+    saveRDS(iso_features, file.path(iso_data_dir, "iso_features.rds"))
+    messagef("Done.")
+
+    messagef("Importing all rhythm data from %s", iso_data_dir)
+    messagef <- function(...) message(sprintf(...))
+    
+    rhythm_data <- read_all_files(rhythm_data_dir, type = "rhythm_prod")
+    saveRDS(rhythm_data, file.path(rhythm_data_dir, "rhythm_data.rds"))
+    
+    messagef("Calculating iso features...")
+    rhythm_features <-  get_rhythm_features(rhythm_data, stimulus_data) 
+    assign("rhythm_features", rhythm_features, globalenv())
+    saveRDS(rhythm_features, file.path(rhythm_data_dir, "rhythm_features.rds"))
+    messagef("Done.")
+    
   }
   else{
-    messagef("Reading all_data_iso.rds")
-    all_data_iso <- readRDS(file.path(iso_data_dir, "all_data_ioi.rds"))
+    messagef("Reading iso data")
+    iso_data <- readRDS(file.path(iso_data_dir, "iso_data.rds"))
+    iso_features <- readRDS(file.path(iso_data_dir, "iso_features.rds"))
+    
+    messagef("Reading rhythm data")
+    rhythm_data <- readRDS(file.path(rhythm_data_dir, "rhythm_data.rds"))
+    rhythm_features <- readRDS(file.path(rhythm_data_dir, "rhythm_features.rds"))
+    
   }
-  assign("all_data_iso", all_data_iso, globalenv())
-  messagef("Calculating iso features...")
-  iso_features <-  get_iso_features(all_data_iso) 
+  assign("iso_data", iso_data, globalenv())
+  assign("rhythm_data", rhythm_data, globalenv())
   assign("iso_features", iso_features, globalenv())
-  messagef("Done.")
-  
-  invisible(all_data_iso)
+  assign("rhythm_features", rhythm_features, globalenv())
+  invisible(list(iso_data, iso_features, rhythm_data, rhythm_features))
 }
